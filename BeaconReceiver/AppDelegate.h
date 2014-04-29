c8a9bdd3936e7af0992ec47ff44f5bdce0f0bd57
cianclarke <cian@cianclarke.com>
2014-04-29 15:01:46 -0400

    Initial Commit

---
+++ BeaconReceiver/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeaconReceiver
+//
+//  Created by Cian Clarke on 29/04/2014.
+//  Copyright (c) 2014 FeedHenry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeaconReceiver/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BeaconReceiver
+//
+//  Created by Cian Clarke on 29/04/2014.
+//  Copyright (c) 2014 FeedHenry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
